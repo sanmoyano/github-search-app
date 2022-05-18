061d22d0e93c4ad56dc143b34b40b23f51e54413
sanmoyano <smoyano.di@gmail.com>
2022-05-18 09:50:38 -0300

    add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Link, Stack, Text } from "@chakra-ui/react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <Stack>
+            <Text color={"gray.500"} fontSize={"xs"}>
+                Made with{" "}
+                <span aria-label="love" role="img">
+                    ❤️
+                </span>{" "}
+                by{" "}
+                {
+                    <Link href="https://smportfolio.vercel.app/" target={"_blank"}>
+                        {" "}
+                        Santiago Moyano
+                    </Link>
+                }{" "}
+                {"- 2022"}
+            </Text>
+        </Stack>
+    );
 };
 
 export default Footer;
